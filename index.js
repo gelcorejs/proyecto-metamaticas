088dcf80d870bb80bc5ae1dfe5763c73ad714ffa
gel <corp.gel@gmail.com>
2019-09-12 17:26:19 -0500

    Primera Version del modulo matematicas

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    /**
+     * Metodo que suma dos Numero
+     * @example
+     * n1=1, n2=2 => resultado = 3
+     * @param {*} n1 numero uno de la suma 
+     * @param {*} n2 numero dos de la suma
+     */
+    suma: function(n1,n2){
+        return n1+n2;
+    },
+    /**
+     * Metodo que Multiplica dos Numero
+     * @example
+     * n1=1, n2=2 => resultado = 2
+     * @param {*} n1 numero uno de la multi 
+     * @param {*} n2 numero dos de la multi
+     */
+    multiplicacion: function(n1,n2){
+        return n1*n2;
+    },
+        /**
+     * Metodo que divide dos Numero
+     * @example
+     * n1=1, n2=2 => resultado = 0.5
+     * @param {*} n1 numero uno de la divide 
+     * @param {*} n2 numero dos de la divide
+     */
+    division: function(){
+        return n1/n2;
+    },
+        /**
+     * Metodo que resta dos Numero
+     * @example
+     * n1=1, n2=2 => resultado = -1
+     * @param {*} n1 numero uno de la resta 
+     * @param {*} n2 numero dos de la resta
+     */
+    resta: function(){
+        return n1-n2;
+    }
+}
---
+++ pruebas.js
@@ -0,0 +1,6 @@
+const m = require('.');
+
+console.log(m.suma(2,5));
+console.log(m.division(2,5));
+console.log(m.resta(2,5));
+console.log(m.multiplicacion(2,5));
